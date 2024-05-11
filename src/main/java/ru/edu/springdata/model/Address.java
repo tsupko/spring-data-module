750eb734e136de77616c527751a16531a88ddf66
Alexander Tsupko <alexander.tsupko@outlook.com>
2024-05-11 21:23:59 +0300

    add work with `Spring Data JPA`

--- src/main/java/ru/edu/springdata/model/Address.java
+++ src/main/java/ru/edu/springdata/model/Address.java
@@ -1,8 +1,35 @@
 package ru.edu.springdata.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Address {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private String city;
+
     private String street;
+
+    public Address() {
+    }
+
+    public Address(String city, String street) {
+        this.city = city;
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                '}';
+    }
 }
--- src/main/java/ru/edu/springdata/model/Category.java
+++ src/main/java/ru/edu/springdata/model/Category.java
@@ -1,10 +1,37 @@
 package ru.edu.springdata.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Категория книг. На пр. Health, History, IT
  */
+@Entity
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private String name;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    private List<Book> books = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
